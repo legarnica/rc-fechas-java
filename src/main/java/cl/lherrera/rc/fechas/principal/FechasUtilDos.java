a3741a90fa1eee82b219b54fbcad467edb24e6c4
Luis Herrera <luis.herrera@tinet.cl>
2021-07-20 16:51:46 -0400

    se implementa el método para pasar una fecha a palabras. refactor

--- src/main/java/cl/lherrera/rc/fechas/principal/FechasUtilDos.java
+++ src/main/java/cl/lherrera/rc/fechas/principal/FechasUtilDos.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
